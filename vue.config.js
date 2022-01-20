6ac8a62f7c21c03d358c67d0f42ed4c0a72bc568
sqooid <lucasliu7@hotmail.com>
2022-01-20 15:52:19 +1100

    Project configured

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,18 @@
-const { defineConfig } = require("@vue/cli-service");
+const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true,
-});
+  configureWebpack: {
+    resolve: {
+      alias: {
+        '@': __dirname + '/src',
+        '@client': __dirname + '/src/client',
+        '@shared': __dirname + '/src/shared',
+      },
+    },
+    entry: {
+      app: './src/client/main.ts',
+    },
+  },
+})
+
+console.log('dir: ', process.cwd())
